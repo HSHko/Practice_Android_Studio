5cae1457bf5759e1a752b107261f4230c4f3436e
HSHko <pleasmin@gmail.com>
2020-08-16 19:06:05 +0900

    20.08.16 kotlin 연습. 버튼만들기

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "btnShowToast Clicked")
+
+            Toast.makeText(this, "btnShowToast Clicked", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+}
